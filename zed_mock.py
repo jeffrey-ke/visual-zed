d72c12c463627bef7da3af982208cdefc9bac5fd
Jeff <jeffke613@gmail.com>
2025-12-15 10:41:11 -0500

    a mock up of zed images so I don't need to use actual hardware

---
+++ zed_mock.py
@@ -0,0 +1,55 @@
+import numpy as np
+import pdb
+import cv2
+
+from . import zed_wrapper
+from model import GeometricServoing
+from datastructs import Intrinsics, CaptureResult, CaptureSequence
+from dataset import StereoImageDataset
+import diagnostics
+from mathutils import hom as make_rgba
+
+class MockZed(zed_wrapper.ZedWrapper):
+    def __init__(self, dataset_path, intrinsics_json="calibs.json"):
+        self.dataset = StereoImageDataset(dataset_path, isIsaacSim=True)
+        self.intrinsics_json = intrinsics_json
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+    def get_K(self):
+        return GeometricServoing._load_intrinsics(self.intrinsics_json).numpy()
+
+    def get_intrinsics(self) -> Intrinsics:
+        K = GeometricServoing._load_intrinsics(self.intrinsics_json)
+        return Intrinsics(fx=K[0,0].item(), fy=K[1,1].item(), cx=K[0,-1].item(), cy=K[1,-1].item())
+
+    def capture_image(self) -> CaptureResult:
+        selected = np.random.choice(self.dataset.samples)
+        left_img = make_rgba(cv2.imread(selected['img_left_path']))
+        right_img = make_rgba(cv2.imread(selected['img_right_path']))
+        depth = np.load(selected['depth_left_path'])
+        depth_rescaled = (
+                (
+                    (depth - depth.min()) / (depth.max() - depth.min()) * 255
+                )
+                .astype(np.uint8)
+        )
+        depth_img =  cv2.applyColorMap(
+            depth_rescaled,
+            cv2.COLORMAP_JET
+        )
+        return CaptureResult(0, 0.0, left_img, right_img, depth, depth_img, True, None)
+
+    def capture_sequence(self, num_captures: int, skip_on_failure: bool = False, delay_between_captures: float = 0) -> CaptureSequence:
+        return None # type: ignore
+
+if __name__ == "__main__":
+
+    diagnostics.start("diagnostics/throwaway")
+    with MockZed("datasets/cornerset") as zm:
+        capt = zm.capture_image()
+        written = cv2.imwrite("file.png", capt.image_left) # type: ignore
